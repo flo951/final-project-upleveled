a21317c209c4156cfc1174fe22e2a854abf6ad19
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-01 14:44:14 +0100

    Add global styles file

---
+++ styles/styles.js
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+export const formContainerStyles = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  margin: 1rem 1rem;
+  /* border-radius: 8px;
+  border: 2px solid black; */
+  /* background-color: #3d86be; */
+  h1 {
+    text-align: center;
+    font-weight: 400;
+  }
+`;
+
+export const formStyles = css`
+  padding: 24px;
+  flex-direction: column;
+  color: black;
+  display: flex;
+  gap: 1rem;
+  margin: 1rem 1rem;
+  border-radius: 8px;
+  h4 {
+    font-size: 24px;
+  }
+`;
