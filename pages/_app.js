2dfeb605146a6808f6c8b8b9e0a3b1e2a06de2e5
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-01 14:45:37 +0100

    Add layout and global component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
+import { css, Global } from '@emotion/react';
+import Layout from '../components/Layout';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            margin: 0;
+            box-sizing: border-box;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+          }
+        `}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
