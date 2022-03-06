73c13cabbabbbaae4bac21a3602ca830830f8f0d
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-06 16:31:18 +0100

    Add matching user_id

--- migrations/1646319989-createTableEvents.js
+++ migrations/1646319989-createTableEvents.js
@@ -2,8 +2,8 @@ exports.up = async (sql) => {
   await sql`
 	 CREATE TABLE events (
 	 	id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	 	eventname varchar(30) NOT NULL UNIQUE
-
+	 	eventname varchar(30) NOT NULL UNIQUE,
+		 user_id integer REFERENCES users (id) ON DELETE CASCADE
 		 );
 `;
 };
