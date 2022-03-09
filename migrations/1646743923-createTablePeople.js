a2f097b7927b6d55945552f2d356fe21cbac455e
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-09 16:11:37 +0100

    Update migration files

--- migrations/1646743923-createTablePeople.js
+++ migrations/1646743923-createTablePeople.js
@@ -3,6 +3,7 @@ exports.up = async (sql) => {
 	 CREATE TABLE people (
 	 	id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	 	name varchar(30) NOT NULL,
+		event_id integer REFERENCES events (id) ON DELETE CASCADE,
 		user_id integer REFERENCES users (id) ON DELETE CASCADE
 
 		 );
