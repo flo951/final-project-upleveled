f91e854f69662ddf72ffbf962a2cd236ba862613
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-09 16:08:35 +0100

    Update api route

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { createEvent, Event, User } from '../../util/database';
+import { createEvent, deleteEventById, Event, User } from '../../util/database';
 
-export type CreateEventResponseBody =
-  | { errors: { message: string }[] }
-  | { event: Event };
+export type CreateEventResponseBody = {
+  event?: Event;
+  errors?: { message: string }[];
+};
 
 type CreateEventRequestBody = {
   eventname: string;
   user: User;
+  eventId?: number;
 };
 
 type CreateEventNextApiRequest = Omit<NextApiRequest, 'body'> & {
@@ -41,6 +43,32 @@ export default async function createEventHandler(
     );
 
     response.status(201).json({ event: event });
+    return;
+  } else if (request.method === 'DELETE') {
+    console.log(request.body);
+    if (
+      typeof request.body.eventId !== 'number' ||
+      !request.body.eventId ||
+      typeof request.body.user.username !== 'string' ||
+      !request.body.user.username
+    ) {
+      // 400 bad request
+      response.status(400).json({
+        errors: [{ message: 'id or event name not provided' }],
+      });
+      return; // Important, prevents error for multiple requests
+    }
+    // if the method is DELETE delete the person matching the id and user_id
+    const deletedEvent = await deleteEventById(
+      request.body.eventId,
+      request.body.user.id,
+    );
+
+    if (!deletedEvent) {
+      response.status(404).json({ errors: [{ message: 'Name not provided' }] });
+      return;
+    }
+    response.status(201).json({ event: deletedEvent });
     return;
   }
 
