26f8f344f77b9a68cdff1c7de6ea83b47b7fd592
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-09 16:09:12 +0100

    Add anchor function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import { css } from '@emotion/react';
+import { css, Interpolation, Theme } from '@emotion/react';
 import { User } from '../util/database';
+import { AnchorHTMLAttributes } from 'react';
 
 const headerStyles = css`
   padding: 12px 12px;
@@ -59,6 +60,15 @@ type Props = {
   userObject?: User;
 };
 
+function Anchor({
+  children,
+  ...restProps
+}: AnchorHTMLAttributes<HTMLAnchorElement> & {
+  css?: Interpolation<Theme>;
+}) {
+  return <a {...restProps}>{children}</a>;
+}
+
 export default function Header(props: Props) {
   return (
     <header css={headerStyles}>
@@ -74,9 +84,7 @@ export default function Header(props: Props) {
           </div>
           <div css={flexContainerStyles}>
             <h3>Hi {props.userObject.username}</h3>
-            <Link href="/logout">
-              <a>Logout</a>
-            </Link>
+            <Anchor href="/logout">Logout</Anchor>
           </div>
         </div>
       ) : (
