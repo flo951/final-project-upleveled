63e6f3e461735ddd0eda785e73dc50c3b0e83594
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-09 16:09:38 +0100

    Create new migration file for expenses table

---
+++ migrations/1646743942-createTableExpenses.js
@@ -0,0 +1,18 @@
+exports.up = async (sql) => {
+  await sql`
+	 CREATE TABLE expenses (
+	 	id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	 	expensename varchar(30) NOT NULL UNIQUE,
+		cost integer,
+		event_id integer REFERENCES events (id) ON DELETE CASCADE,
+		paymaster integer
+
+		 );
+`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+	DROP TABLE expenses
+	`;
+};
