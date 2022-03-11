65c6d96efe698f41d0a39bcd20c995e48a7dfcdb
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-11 16:23:17 +0100

    Change max age to 24 hours like session token in db

--- util/cookies.ts
+++ util/cookies.ts
@@ -4,7 +4,7 @@ export function createSerializedRegisterSessionTokenCookie(token: string) {
   // check if we are in production e.g. Heroku
   const isProduction = process.env.NODE_ENV === 'production';
 
-  const maxAge = 60 * 100; // 100 minutes
+  const maxAge = 60 * 60 * 24; // 24 hours
 
   return serialize('sessionToken', token, {
     maxAge: maxAge,
