eecd3d472d76a05579b1ea768c62247f7c12711e
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-13 17:07:40 +0100

    Change expense name to be not unique

--- migrations/1646743942-createTableExpenses.js
+++ migrations/1646743942-createTableExpenses.js
@@ -2,7 +2,7 @@ exports.up = async (sql) => {
   await sql`
 	 CREATE TABLE expenses (
 	 	id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	 	expensename varchar(30) NOT NULL UNIQUE,
+	 	expensename varchar(30) NOT NULL,
 		cost integer,
 		event_id integer REFERENCES events (id) ON DELETE CASCADE,
 		paymaster integer
