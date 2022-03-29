d29490f012d01cabfd619f75f3631ccf12853682
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-29 11:46:20 +0200

    Switch file from js to tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,47 @@
+import { css, Global } from '@emotion/react';
+import { AppProps } from 'next/app';
+import { useCallback, useEffect, useState } from 'react';
+import Layout from '../components/Layout';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [user, setUser] = useState();
+
+  const refreshUserProfile = useCallback(async () => {
+    const response = await fetch('/api/profile');
+    const data = await response.json();
+
+    if ('errors' in data) {
+      console.log(data.errors);
+      setUser(undefined);
+      return;
+    }
+
+    setUser(data.user);
+  }, []);
+
+  useEffect(() => {
+    refreshUserProfile().catch(() => {});
+  }, [refreshUserProfile]);
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body {
+            margin: 0;
+            box-sizing: border-box;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+          }
+        `}
+      />
+
+      <Layout userObject={user}>
+        <Component {...pageProps} refreshUserProfile={refreshUserProfile} />
+      </Layout>
+    </>
+  );
+}
+
+export default MyApp;
