07a36dbbf7ac2b1eaf5dc04bac76176d330c2bcc
Florian Görlich <florian.goerlich95@gmail.com>
2022-03-29 11:44:32 +0200

    Remove refreshuserpofile from props

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { User } from '../util/database';
 import Header from './Header';
 type Props = {
@@ -6,10 +7,16 @@ type Props = {
 };
 export default function Layout(props: Props) {
   return (
-    <div>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/images/icon-apple-touch.png" />
+        <link rel="icon" href="images/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="images/icon-192.png" type="image/svg+xml" />
+      </Head>
       <Header userObject={props.userObject} />
 
       <main>{props.children}</main>
-    </div>
+    </>
   );
 }
