fc06bccdc1dc8357186b95a4363be51bdc6b7a83
Florian Görlich <florian.goerlich95@gmail.com>
2022-04-11 20:48:31 +0200

    Remove old code

--- pages/users/[eventId].tsx
+++ pages/users/[eventId].tsx
@@ -19,7 +19,6 @@ import {
 } from '../../util/database';
 import { CreateEventResponseBody, DeleteEventResponseBody } from '../api/event';
 import { DeleteExpenseResponseBody } from '../api/expense';
-
 import { Errors, formStyles, spanStyles } from '../createevent';
 import { errorStyles } from '../login';
 
@@ -273,40 +272,6 @@ export default function UserDetail(props: Props) {
     );
   }
 
-  // // function to delete created people
-  // async function deletePerson(id: number) {
-  //   const deleteResponse = await fetch(`/api/person`, {
-  //     method: 'DELETE',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       personId: id,
-  //       user: props.user,
-  //     }),
-  //   });
-  //   const deletePersonResponseBody =
-  //     (await deleteResponse.json()) as DeletePersonResponseBody;
-
-  //   if ('errors' in deletePersonResponseBody) {
-  //     setErrors(deletePersonResponseBody.errors);
-  //     return;
-  //   }
-  //   if ('person' in deletePersonResponseBody) {
-  //     const newPeopleList = peopleList.filter((person) => {
-  //       return deletePersonResponseBody.person.id !== person.id;
-  //     });
-  //     setPeopleList(newPeopleList);
-
-  //     const newExpenseList = expenseList.filter((expense) => {
-  //       return deletePersonResponseBody.person.id !== expense.paymaster;
-  //     });
-  //     setExpenseList(newExpenseList);
-
-  //     return;
-  //   }
-  // }
-
   async function deleteExpense(id: number) {
     const deleteResponse = await fetch(`/api/expense`, {
       method: 'DELETE',
@@ -531,91 +496,6 @@ export default function UserDetail(props: Props) {
                   setExpenseList={setExpenseList}
                   eventId={props.eventInDb.id}
                 />
-                {/* <form
-                  onSubmit={async (e) => {
-                    e.preventDefault();
-
-                    const createPersonResponse = await fetch('/api/person', {
-                      method: 'POST',
-                      headers: {
-                        'Content-Type': 'application/json',
-                      },
-                      body: JSON.stringify({
-                        name: personName,
-                        user: props.user,
-                        eventId: event.id,
-                      }),
-                    });
-
-                    const createPersonResponseBody =
-                      (await createPersonResponse.json()) as DeletePersonResponseBody;
-                    if ('person' in createPersonResponseBody) {
-                      const createdPeople = [
-                        ...peopleList,
-                        createPersonResponseBody.person,
-                      ];
-                      setPeopleList(createdPeople);
-
-                      setPersonName('');
-                      return;
-                    }
-
-                    if ('errors' in createPersonResponseBody) {
-                      setErrors(createPersonResponseBody.errors);
-                      return;
-                    }
-                  }}
-                  css={formStyles}
-                >
-                  <label htmlFor="person-name">Name of Person</label>
-                  <input
-                    css={nameInputStyles}
-                    data-test-id="create-person"
-                    id="person-name"
-                    placeholder="Name"
-                    value={personName}
-                    onChange={(e) => setPersonName(e.currentTarget.value)}
-                    required
-                  />
-
-                  <input
-                    css={inputSubmitStyles}
-                    data-test-id="complete-create-person"
-                    type="submit"
-                    value="Add Person"
-                  />
-                </form> */}
-                {/* <div css={divPersonListStyles}>
-                  {peopleList.map((person: Person) => {
-                    return (
-                      person.eventId === event.id && (
-                        <div
-                          data-test-id={`person-width-id-${person.id}`}
-                          key={`this is ${person.name} witdh ${person.id} from event ${event.id}`}
-                        >
-                          <div css={personStyles}>
-                            <span
-                              css={spanStyles}
-                              data-test-id={`name-${person.name}`}
-                              data-id={person.id}
-                            >
-                              {person.name}
-                            </span>
-                            <button
-                              css={removeButtonStyles}
-                              aria-label={`Delete Button for Person: ${person.name}`}
-                              onClick={() => {
-                                deletePerson(person.id).catch(() => {});
-                              }}
-                            >
-                              X
-                            </button>
-                          </div>
-                        </div>
-                      )
-                    );
-                  })}
-                </div> */}
                 {errors && (
                   <div css={errorStyles}>
                     {errors.map((error) => {
