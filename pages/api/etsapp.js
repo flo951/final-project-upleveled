b97931b8b368fee16256532e5cc071b0bd197893
Florian.Goerlich <florian.goerlich@enio.at>
2022-08-18 11:37:46 +0200

    Edit data

--- pages/api/etsapp.js
+++ pages/api/etsapp.js
@@ -7,16 +7,16 @@ export default function createAppHandler(request, response) {
 
     const userData = {
         username: request.body.username,
-        carAssigned: "BMW-X6",
+        carAssigned: "W-92345",
        carPool: [
-        { label: "Toyota", value: "toyota", plate: "W-92345" },
-        { label: "BMW", value: "bmw", plate: "W-82345" },
-        { label: "Nissan", value: "nissan", plate: "W-02345" },
-        { label: "Aston Martin", value: "aston martin", plate: "W-12345" },
-        { label: "Ferrari", value: "ferrari", plate: "W-22345" },
-        { label: "Lamborghini", value: "lamborghini", plate: "W-32345" },
-        { label: "Mercedes", value: "mercedes", plate: "W-52345" },
-        { label: "VW", value: "vw", plate: "W-42345" },
+        { label: "W-92345", value: "toyota" },
+        { label: "W-82345", value: "bmw" },
+        { label: "W-02345", value: "nissan"},
+        { label: "W-12345", value: "aston martin"  },
+        { label: "W-22345", value: "ferrari" },
+        { label: "W-32345", value: "lamborghini" },
+        { label: "W-52345", value: "mercedes" },
+        { label: "W-42345", value: "vw" },
       ],
 
 
