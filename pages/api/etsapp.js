b3717c870e4dd64a2de2024a9575b00c5c53718e
Florian.Goerlich <florian.goerlich@enio.at>
2022-08-18 12:05:04 +0200

    change

--- pages/api/etsapp.js
+++ pages/api/etsapp.js
@@ -2,28 +2,31 @@
 
 export default function createAppHandler(request, response) {
   if (request.method === 'POST') {
+    console.log(request.body.username)
+    if(request.body.username === "Rohrdorfer") {
+        const userData = {
+            username: request.body.username,
+            carAssigned: "vw",
+           carPool: [
+            { label: "W-92345", value: "toyota" },
+            { label: "W-82345", value: "bmw" },
+            { label: "W-02345", value: "nissan"},
+            { label: "W-12345", value: "aston martin"  },
+            { label: "W-22345", value: "ferrari" },
+            { label: "W-32345", value: "lamborghini" },
+            { label: "W-52345", value: "mercedes" },
+            { label: "W-42345", value: "vw" },
+          ],
     
     
-
-    const userData = {
-        username: request.body.username,
-        carAssigned: "W-42345",
-       carPool: [
-        { label: "W-92345", value: "toyota" },
-        { label: "W-82345", value: "bmw" },
-        { label: "W-02345", value: "nissan"},
-        { label: "W-12345", value: "aston martin"  },
-        { label: "W-22345", value: "ferrari" },
-        { label: "W-32345", value: "lamborghini" },
-        { label: "W-52345", value: "mercedes" },
-        { label: "W-42345", value: "vw" },
-      ],
-
-
+        }
+        console.log(userData)
+       response.status(200).json({ userData: userData });
+        return;
     }
+    
 
-   response.status(200).json({ userData: userData });
-    return;
+    
   }
 
   response.status(405).json({ errors: [{ message: 'Method not supported' }] });
